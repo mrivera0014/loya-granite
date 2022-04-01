b4de34fdceec6f4d2ba26c7b126ae23da0015d7b
chris watkins <chrisbwatkins@gmail.com>
2022-04-01 11:41:22 -0600

    the project animations

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, {useRef,useEffect} from 'react'
+import {assists} from './projectAssets/projectAssists';
+import  './projects.css'
+import {gsap} from "gsap/all"
 
-const projects = () => {
+
+
+
+const Projects = () => {
+
+
+
+
+  const projectElRef = gsap.utils.toArray(".projectItems")
+
+  projectElRef.map((item,index)=>{
+    item.anim = gsap.to(item,{
+      height:100
+    }).reversed(true);
+    item.addEventlistener("click",doStuff)
+  })
+
+  function doStuff(){
+    this.anim.reversed(!this.anim.reversed())
+  }
+
   return (
-    <div>projects</div>
+  <div className={`projects`}>
+   {assists.map((item,index)=>{
+    return(
+      <button className={`projectItems`}  >
+        <h1>{item.item}</h1>
+      </button>
+    )
+   })}
+  </div>
+
+
+
+
+
+
+
+
   )
 }
 
-export default projects
+export default Projects
---
+++ src/Components/Projects/projectAssets/projectAssists.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const assists = [
+
+    {item:"one"},
+    {item:"two"},
+    {item:"three"},
+    {item:"four"},
+    {item:"five"},
+    {item:"six"},
+    {item:"seven"},
+    {item:"eight"},
+    {item:"nine"},
+    {item:"ten"}
+
+]
