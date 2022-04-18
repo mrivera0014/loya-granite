53b4cd2f6b56811af699ca9bc00440686f4e7c61
Marble <mrivera0014@gmail.com>
2022-04-17 21:31:37 -0600

    created contact page, tab functional

---
+++ src/Components/About/About.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './About.css'
+
+const About = () => {
+    return (
+        <div>
+            <h3 className='loyaGranite'>ABOUT LOYA GRANITE</h3>
+            <p>Loya Granite is located in the Denver, Co. We strive to provide Colorado with affordable and professional granite installation. </p>
+
+            <p>We cover everything from kitchens, baths, fireplaces, reception areas and more!</p>
+
+            <p>We provide granite stone, quartz, marble, solid stone, etc. If you have any questions please do not hesitate to contact us! </p>
+        </div>
+    )
+}
+
+export default About
