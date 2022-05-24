42e61c297ac6674a12b703cbf1a7a8c72b2cee5f
Marble <mrivera0014@gmail.com>
2022-05-23 18:39:05 -0600

    working on making functional contact form

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const app = express();
+// const app = express();
 require('dotenv').config();
-const cors = require('cors');
+// const cors = require('cors');
 const UseCalc = require('./data/calculations');
 const { config } = require('dotenv');
 const answers = [];
@@ -21,49 +21,139 @@ initializePassport(
 
 
 
-app.set('view-engine', 'ejs');
-app.use(express.urlencoded({ extended: false }))
-app.use(flash());
-app.use(session({
-    secret: process.env.SESSION_SECRET,
-    resave: false,
-    saveUninitialized: false
-}))
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(methodOverride("_method"))
-app.get('/', checkNotAuthenticated, (req, res) => {
-    res.render("./answer.ejs");
-})
+// app.set('view-engine', 'ejs');
+// app.use(express.urlencoded({ extended: false }))
+// app.use(flash());
+// app.use(session({
+//     secret: process.env.SESSION_SECRET,
+//     resave: false,
+//     saveUninitialized: false
+// }))
+// app.use(passport.initialize());
+// app.use(passport.session());
+// app.use(methodOverride("_method"))
+// app.get('/', checkNotAuthenticated, (req, res) => {
+//     res.render("./answer.ejs");
+// })
 
-app.get("/stuff", checkNotAuthenticated, (req, res) => {
-    res.render('login.ejs')
-})
-
-
+// app.get("/stuff", checkNotAuthenticated, (req, res) => {
+//     res.render('login.ejs')
+// })
 
 
-app.post('/calc', (req, res) => {
-    let result = new UseCalc(Number(req.body.unitOne), Number(req.body.unitTwo));
-    answers.push(result.yep);
-    console.log(answers);
-    res.redirect('/answer');
-})
 
 
-function checkAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next()
+// app.post('/calc', (req, res) => {
+//     let result = new UseCalc(Number(req.body.unitOne), Number(req.body.unitTwo));
+//     answers.push(result.yep);
+//     console.log(answers);
+//     res.redirect('/answer');
+// })
+
+
+// function checkAuthenticated(req, res, next) {
+//     if (req.isAuthenticated()) {
+//         return next()
+//     }
+
+//     res.redirect('/')
+// }
+
+// function checkNotAuthenticated(req, res, next) {
+//     if (req.isAuthenticated()) {
+//         return res.redirect('/')
+//     }
+//     next()
+// }
+
+
+
+
+// contact form
+const router = express.Router()
+const nodemailer = require('nodemailer')
+const cors = require('cors');
+const { getDefaultNormalizer } = require('@testing-library/react');
+
+const app = express()
+const port = process.env.PORT || 3001;
+
+app.use('/', router)
+app.listen(port, () => console.log('connected'))
+
+const contactSend = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    aut: {
+        user: 'mrivera0014@gmail.com',
+        pass: 'Penny!14'
     }
+});
 
-    res.redirect('/')
-}
-
-function checkNotAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return res.redirect('/')
+app.post('/', (req, res) => {
+    const name = req.body.name;
+    const email = req.body.email;
+    const phone = req.body.phone;
+    const message = req.body.message;
+    const mail = {
+        from: name,
+        to: 'mrivera0014@gmail.com',
+        Subject: `Message From ${name}`,
+        html: `
+        <p>Name: ${name}</p>
+        <p>Email: ${email}</p>
+        <p>Phone: ${phone}</p>
+        <p>Message: ${message}</p>`
     }
-    next()
-}
+    console.log(req)
+    contactSend.sendMail(mail, (error) => {
+        console.log(mail);
+        if (error) {
+            res.json({ status: 'failed' });
+        } else {
+            res.json({
+                status: 'sent'
+            })
+        }
+    })
+})
+
+
 
 app.listen(3050)
+
+// app.post('/api/contact', (req, res) => {
+//     let data = req.body
+//     let smtpTransport = nodemailer.createTransport({
+//         service: 'Gmail',
+//         port: 587,
+//         auth: {
+//             user: 'mrivera0014@gmail.com',
+//             pass: 'Penny!14'
+//         }
+//     })
+// })
+
+// let mailOptions = {
+//     from: data.email,
+//     to: 'mrivera0014@gmail.com',
+//     subject: `Message from ${data.name}`,
+//     html:
+//         `<h3>Contact Info For Customer</h3>
+//     <p>Name: ${data.name}</p>
+//     <p>Email: ${data.email}</p>
+//     <p>Phone: ${data.phone}</p>
+//     <p>Message: ${data.message}</p>
+//     `
+// }
+
+
+// smtpTransport.contactSend(mailOptions, (err, res) => {
+//     if (err) {
+//         res.send(err)
+//     }
+//     else {
+//         res.send('Success')
+//     }
+//     smtpTransport.close()
+// })
--- src/Components/Contact/Contact.js
+++ src/Components/Contact/Contact.js
@@ -1,31 +1,108 @@
-import React from 'react'
+import React, { Component } from 'react'
 import './Contact.css'
 
-const Contact = () => {
-    return (
-        <div className='contactContain'>
-            {/* creating contact form */}
-            <form id='form' className='form'>
-                <h3>Let's talk about your project</h3>
-                <input placeholder='Name' className='name' />
-                <input placeholder='Email' className='email' />
-                <textarea placeholder='Message' className='message' />
-                <input id='submit' type='submit' className='submit' />
-            </form>
+import axios from 'axios'
 
-            <section className='contactInfo'>
-                <h3>Contact Info</h3>
-                <p>Phone: (661) 466-3286</p>
-                <p>Email: dieguito.loyagranite19@gmail.com</p>
-            </section>
-            <section className='loyaHours'>
-                <h3>Hours of Operation</h3>
-                <p>Monday-Friday: 9am- 5pm</p>
-                <p>Saturday: Closed</p>
-                <p>Sunday: Closed</p>
-            </section>
-        </div>
-    )
+class Contact extends Component {
+
+    state = {
+        name: '',
+        email: '',
+        phone: '',
+        message: '',
+        sent: false
+    }
+
+    handleName = (e) => {
+        this.setState({
+            name: e.target.value
+        })
+    }
+    handleEmail = (e) => {
+        this.setState({
+            email: e.target.value
+        })
+    }
+    handlePhone = (e) => {
+        this.setState({
+            phone: e.target.value
+        })
+    }
+    handleMessage = (e) => {
+        this.setState({
+            message: e.target.value
+        })
+    }
+
+
+    contactSend(e) {
+        console.log('contact send working')
+        e.preventDefault();
+        console.log('prevent working')
+        let data = {
+            name: this.state.name,
+            email: this.state.email,
+            phone: this.state.phone,
+            message: this.state.message
+        }
+
+        axios.post('api/contact', data)
+            .then(res => {
+                this.setState({
+                    sent: true,
+                }, this.resetForm())
+            }).catch(() => {
+                console.log('message not sent')
+            })
+    }
+
+
+    resetForm = () => {
+        this.setState({
+            name: '',
+            email: '',
+            phone: '',
+            message: '',
+        })
+
+        setTimeout(() => {
+            this.setState({
+                sent: false,
+            })
+        }, 3000);
+    }
+
+
+
+    render() {
+        return (
+            <div className='contactContain' >
+                {/* creating contact form */}
+                <form form id='form' className='form' >
+                    <h3 className='formTitle'>Let's talk about your project</h3>
+                    <input placeholder='Name' id='name' value={this.state.name} onChange={this.handleName} />
+                    <input placeholder='Email' id='email' value={this.state.email} onChange={this.handleEmail} />
+                    <input placeholder='Phone Number' id='phone' value={this.state.phone} onChange={this.handlePhone} />
+                    <textarea placeholder='Message' id='message' value={this.state.message} onChange={this.handleMessage} />
+                    <button id='submit' type='submit' onSubmit={this.contactSend} />
+                </form>
+
+                <section className='contactInfo'>
+                    <h3>Contact Info</h3>
+                    <p>Phone: (661) 466-3286</p>
+                    <p>Email: dieguito.loyagranite19@gmail.com</p>
+                </section>
+                <section className='loyaHours'>
+                    <h3>Hours of Operation</h3>
+                    <p>Monday-Friday: 9am- 5pm</p>
+                    <p>Saturday: Closed</p>
+                    <p>Sunday: Closed</p>
+                </section>
+            </div >
+        )
+    }
+
+
 }
 
 export default Contact
