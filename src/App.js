0e1c72cd5a9b578ce1a7155c868b66c91e496b0e
Marble <mrivera0014@gmail.com>
2022-12-03 21:49:14 -0700

    add react-router-dom, create pages folder, reconstruct app.js to prepare for new navbar

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 import './App.css';
 import Nav from './Components/Nav/Nav'
+import Home from './pages/Home/Home';
+import Projects from './pages/Projects/Projects';
 import Modal from './Components/Modal/Modal'
 import Footer from './Components/Footer/Footer';
-import Splash from "./Components/Splash/splash";
 function App() {
   return (
-    <div >
-      {/* <Splash /> */}
+    <BrowserRouter>
+
       <Modal />
       <Nav />
+      <Routes>
+        <Route path="/" element={<Home />} ></Route>
+        <Route path="/projects" element={<Projects />} ></Route>
+
+      </Routes>
       <Footer />
 
-    </div>
+    </BrowserRouter>
   );
 }
 
