7b6c3c8fc2395a265ee45275dcd60f8a13b9ec27
chris watkins <chrisbwatkins@gmail.com>
2023-02-05 12:28:50 -0700

    updating the projects.

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -2,6 +2,7 @@ import React, { useState, useRef, useEffect } from 'react'
 import { assists, itemOne, itemTwo, itemThree } from './projectAssets/projectAssists';
 import './projects.css'
 import { gsap } from "gsap/all"
+import e from 'cors';
 
 
 
@@ -10,13 +11,13 @@ const ProjectsView = () => {
 
   const [projectToggle, setProjectToggle] = useState(false);
   const [modalToggle, setModalToggle] = useState("");
+  const [bigPicToggle,setBigPicToggle] = useState("");
 
 
 
   let kitchen = itemOne;
   let bathroom = itemTwo;
   let firePlace = itemThree;
-
 
 
 
@@ -40,51 +41,65 @@ const ProjectsView = () => {
     }
 
   }
+  let bigPicFunction = (item) =>{
+  
+    setBigPicToggle(item.itemitem);
+    
+  }
 
-  useEffect(() => {
-    gsap.from('.projects', { duration: 2.5, y: '-100', ease: 'power3.out', opacity: 0 })
-  })
+  // useEffect(() => {
+  //   gsap.from('.projects', { duration: 2.5, y: '-100', ease: 'power3.out', opacity: 0 })
+  // })
 
 
   return (
 
     <div className={`projects`}>
+        <img className='bigPicture' src={bigPicToggle}></img>
+
+      {/* mapping the projects */}
+
+      <div className='projectsDisc'>
+
       <section className='projectsInfo'>
         <p>At Loya Granite we take pride in our work and are not afraid to take on a challenge. We look forward to collaborating with out customers to develop a plan that will ensure customer satisfaction.</p>
       </section>
       <section className='projectsInfo2'>
         <p>Choose from materials such as granite, quartz, marble, and much more! </p>
       </section>
-
-      {/* mapping the projects */}
+      </div>    
+      <div className='photoCollection'>
+      <div className='photoMenu'>
       {assists.map((item, index) => {
 
-        return (
-          <div className='projects_photos' key={index}>
-            <button className={`projectItems`} onClick={() => openModal(index)}>
-              <h1 className={`project_name`}>{item.name}</h1>
-              <img src={item.item} className={`project_item-img`}></img>
-            </button>
-            <div>
-              {/* mapping the modal pictures */}
-            </div>
-          </div>
-        )
+return (
+  <div className='projects_photos' key={index}>
+    <button className={`projectItems`} onClick={() => openModal(index)}>
+      <h1 className={`project_name`}>{item.name}</h1>
+    </button>
+
+  </div>
+)
 
 
-      })}
+})}
+     </div>
 
       <div className={`modal ${projectToggle ? "show" : ""}`}>
 
         <div className={`modal-pictures`}>
-          <button className={`close-button`} onClick={() => closeModal(projectToggle)}>X</button>
+
+
+                
 
           <div className={`${modalToggle == "kitchen" ? "show" : "hide"}`}>
-            <h2 className={`modal-header`}>Kitchens</h2>
+
             <div className={`modal-pictures-sub`}>
               {kitchen.map((item, index) => {
                 return (
+                  <div className={`picture-container`} onClick={() => bigPicFunction(item)}>
                   <img className={`modal-pictures-picture`} src={item.itemitem}  ></img>
+                  </div>
                 )
               }
               )
@@ -92,24 +107,29 @@ const ProjectsView = () => {
             </div>
           </div>
           <div className={` ${modalToggle == "bathroom" ? "show" : "hide"}`}>
-            <h2 className={`modal-header`}>Bathroom</h2>
+
             <div className={`modal-pictures-sub`}>
 
               {bathroom.map((item, index) => {
                 return (
+                  <div className={`picture-container`} onClick={() => bigPicFunction(item)}>
                   <img className={`modal-pictures-picture`} src={item.itemitem}  ></img>
+                  </div>
                 )
               }
               )
               } </div>
           </div>
           <div className={` ${modalToggle == "fireplace" ? "show" : "hide"}`}>
-            <h2 className={`modal-header`}>Fire Places</h2>
+
             <div className={`modal-pictures-sub`}>
 
               {firePlace.map((item, index) => {
                 return (
+                  
+                  <div className={`picture-container`} onClick={() => bigPicFunction(item)}>
                   <img className={`modal-pictures-picture`} src={item.itemitem}  ></img>
+                  </div>
                 )
               }
               )
@@ -120,6 +140,7 @@ const ProjectsView = () => {
 
       </div>
     </div >
+    </div>
   )
 }
 
