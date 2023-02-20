235694f46d17e232d722252106ef0741fce82378
Marble <mrivera0014@gmail.com>
2023-02-19 23:00:42 -0700

    slider fully functional and responsive

--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -11,11 +11,39 @@ export default class Reviews extends Component {
     render() {
         const settings = {
             dots: true,
-            infinite: true,
+            infinite: false,
             speed: 500,
             slidesToShow: 3,
-            slidesToScroll: 3
-        }
+            slidesToScroll: 4,
+            initialSlide: 0,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                        infinite: true,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                        initialSlide: 2
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        };
+
         return (
             <div>
                 <Slider {...settings}>
