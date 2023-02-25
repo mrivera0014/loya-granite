4eb1ebbe093d1bd5eeb98d537ca5a2fc21cd04ed
Marble <mrivera0014@gmail.com>
2023-02-24 21:22:14 -0700

    review cards styled

--- src/Components/ReviewItems/ReviewItems.js
+++ src/Components/ReviewItems/ReviewItems.js
@@ -1,3 +1,5 @@
+import { BsFillStarFill } from 'react-icons/bs'
+
 export const reviewItems = [
 
 
@@ -7,8 +9,9 @@ export const reviewItems = [
         sourceURL: 'https://www.homeadvisor.com/rated.Loyagranite.109184108.html',
         firstName: 'Christina',
         lastName: 'Santiago',
-        date: '01-13-2023',
-        ratingValue: 5,
+        date: '01/13/2023',
+        ratingValue: '5.0',
+        reviewIcon: <BsFillStarFill />,
         reviewTitle: 'Install Laminate Countertops',
         review: 'Very professional to work with. Finished product is beautiful and I look forward to using them on our next project.'
     },
@@ -18,8 +21,9 @@ export const reviewItems = [
         sourceURL: 'https://www.homeadvisor.com/rated.Loyagranite.109184108.html',
         firstName: 'Sandy',
         lastName: 'K',
-        date: '01-13-2023',
-        ratingValue: 5,
+        date: '01/13/2023',
+        ratingValue: '5.0',
+        reviewIcon: <BsFillStarFill />,
         reviewTitle: 'Install Solid Surface Countertops',
         review: 'Amazing Company! They were incredibly easy to work with and punctual. They had great communication skills and did a beautiful job. I would highly recommend them!'
     },
@@ -29,8 +33,9 @@ export const reviewItems = [
         sourceURL: 'https://www.homeadvisor.com/rated.Loyagranite.109184108.html',
         firstName: 'Ann',
         lastName: 'Roeber',
-        date: '01-06-2023',
-        ratingValue: 5,
+        date: '01/06/2023',
+        ratingValue: '5.0',
+        reviewIcon: <BsFillStarFill />,
         reviewTitle: 'Install Stone Slab Countertops',
         review: 'This is THE place to go for countertops! Absolutely wonderful customer service. Fast, continual contact, and amazing installation. We couldn\'t be happier!'
     },
@@ -40,8 +45,9 @@ export const reviewItems = [
         sourceURL: 'https://www.homeadvisor.com/rated.Loyagranite.109184108.html',
         firstName: 'Stefani',
         lastName: 'W',
-        date: '12-02-2022',
-        ratingValue: 5,
+        date: '12/02/2022',
+        ratingValue: '5.0',
+        reviewIcon: <BsFillStarFill />,
         reviewTitle: 'Install Solid Surface Countertops',
         review: 'I am very grateful for the amazing job they did in my kitchen.'
     },
@@ -51,8 +57,9 @@ export const reviewItems = [
         sourceURL: 'https://www.homeadvisor.com/rated.Loyagranite.109184108.html',
         firstName: 'Kenja',
         lastName: 'S',
-        date: '12-02-2022',
-        ratingValue: 5,
+        date: '12/02/2022',
+        ratingValue: '5.0',
+        reviewIcon: <BsFillStarFill />,
         reviewTitle: 'Remodel a Bathroom',
         review: 'They were on time, they were very professional, answered every question and the results were wonderful.'
     },
@@ -62,8 +69,9 @@ export const reviewItems = [
         sourceURL: 'https://www.homeadvisor.com/rated.Loyagranite.109184108.html',
         firstName: 'Natalie',
         lastName: 'M.',
-        date: '11-30-2022',
-        ratingValue: 5,
+        date: '11/30/2022',
+        ratingValue: '5.0',
+        reviewIcon: <BsFillStarFill />,
         reviewTitle: 'Install Laminate Countertops',
         review: 'His company was very professional, they were extremely easy to work with, went above and beyond and did an excellent job on our kitchen. I would definitely use them again.'
     },
--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -20,8 +20,8 @@ export default class Reviews extends Component {
                 {
                     breakpoint: 1024,
                     settings: {
-                        slidesToShow: 3,
-                        slidesToScroll: 3,
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
                         infinite: true,
                         dots: true
                     }
@@ -46,15 +46,24 @@ export default class Reviews extends Component {
 
         return (
             <div>
+                <h2>Reviews</h2>
                 <Slider {...settings}>
                     {reviewItems.map((item, index) => {
                         return (
                             <div {...settings}>
                                 <div className={item.cName} key={item.reviewID}>
-                                    <h4 className='reviewName'><BsFillStarFill className='starRating' /> {item.ratingValue} {item.firstName} {item.lastName} {item.date}</h4>
-                                    <h5 className='reviewTitle'>{item.reviewTitle}</h5>
-                                    <p className='review'>{item.review}</p>
-                                    <a target='_blank' src={item.sourceURL}>Home Advisor</a>
+                                    <div className='cardTop'>
+                                        <a target='_blank' href={item.sourceURL} className='advisorLink'>Home Advisor</a>
+
+                                        <h4 className='reviewName'><BsFillStarFill className='starIcon' /> {item.ratingValue} {item.firstName} {item.lastName}</h4>
+
+                                        <h5 className='reviewTitle'>{item.reviewTitle}</h5>
+                                        <p className='reviewDate'>{item.date}</p>
+                                    </div>
+                                    <div className='cardBody'>
+                                        <p className='review'>{item.review}</p>
+                                    </div>
+
 
                                 </div>
                             </div>
