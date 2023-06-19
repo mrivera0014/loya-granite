7c58d58fa069f18522a27825668a5dba888dc372
chris watkins <chrisbwatkins@gmail.com>
2023-06-19 14:43:24 -0600

    adding mobile responsiveness to our materials page

--- src/pages/Materials/material.js
+++ src/pages/Materials/material.js
@@ -2,25 +2,30 @@ import React from 'react'
 import { materials } from './materialAssets'
 import "./material.css"
 import { Parallax } from 'react-parallax';
+import { idealList } from './materialAssets';
 
 // import quartz from '../../Components/modal/assets/quartz-texture-white.jpg';
 
 const material = () => {
   return (
     <div>
-      <div>Material</div>
+      <div className='pageTitle'>Materials</div>
     <div>{materials.map((item,index)=>{
       return (<>
       <Parallax bgImage={item.itemCoverExample}  bgImageAlt='materialpicture granite' strength={500} className='image'></Parallax>
         <div className='materials_container'>
         <div className='materials_subContainer'>
       <div className='  materials_subContainer-name'>{item.itemName}</div>
-      <div className='  materials_subContainer-ideal'>{item.itemIdeal}</div>
+      <div className="materials_subGrid">
+      <div className='  materials_subContainer-ideal'>{item.itemIdeal}<div></div></div>
       <div className='  materials_subContainer-desc' >{item.itemDesc}</div>
       <div className='  materials_subContainer-seal' >{item.itemSealRequirement}</div>
       <div className='  materials_subContainer-outside'>{item.itemOutsideCapability}</div>
       <div className='  materials_subContainer-value'>{item.itemValue}</div>
       <div className='  materials_subContainer-valueDesc'>{item.itemValueDesc}</div>
+      <div className='  materials_subContainer-maintenance'>{item.itemMaintenance}</div>
+      </div>
+      
       </div>
       <div className='materials_subContainer-example'>
       <div className='materials_subContainer-item '>Example</div>
