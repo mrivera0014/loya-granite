2b794c6e59b4044bae1e57286168beccebe3e58c
Marble <mrivera0014@gmail.com>
2023-09-08 12:22:35 -0600

    fixed stretched logos in footer and mobile nav

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -31,8 +31,8 @@ const footer = () => {
           })}
         </section>
         <a className='haLogo' href="http://www.homeadvisor.com/rated.Loyagranite.109184108.html" target='_blank'><img alt="Loya Granite & Design - Reviews on Home Advisor" src="http://www.homeadvisor.com/images/sp-badges/soap-solid-border.png?sp=109184108&key=e9fdc08ab40001e2c4515b1539c8e690" /></a>
-        <img className='haLogo' alt="Loya Granite & Design - Reviews on Home Advisor" src="https://cdn1.homeadvisor.com/images/sp-badges/toprated.png" />
-        <img className='haLogo' alt="Loya Granite & Design - Reviews on Home Advisor" src="https://cdn1.homeadvisor.com/images/sp-badges/elite.png" />
+        <img className='haLogo topRated' alt="Loya Granite & Design - Reviews on Home Advisor" src="https://cdn1.homeadvisor.com/images/sp-badges/toprated.png" />
+        <img className='haLogo eliteService' alt="Loya Granite & Design - Reviews on Home Advisor" src="https://cdn1.homeadvisor.com/images/sp-badges/elite.png" />
         <section className='socialMedia'>
           <p className='facebook'>Follow us on Facebook :</p>
           <a
